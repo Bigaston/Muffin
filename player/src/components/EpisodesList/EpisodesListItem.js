bf9031f012bc6684b7d3857926ac3e86fa454ed1
Bigaston <bigaston1@gmail.com>
2020-11-16 22:40:42 +0100

    Tada

--- player/src/components/EpisodesList/EpisodesListItem.js
+++ player/src/components/EpisodesList/EpisodesListItem.js
@@ -7,32 +7,34 @@ import { useRecoilState } from "recoil";
 
 import { convertHMS } from "../../utils";
 
+import config from "../../config.json";
+
 const EpisodesListItem = ({ episode, setCurrentEpisode }) => {
-  const [playerState] = useRecoilState(playerStore);
+	const [playerState] = useRecoilState(playerStore);
 
-  const { src: playerSrc, playing, playPause } = playerState;
+	const { src: playerSrc, playing, playPause } = playerState;
 
-  const currently_me = episode.enclosure_url === playerSrc;
+	const currently_me = episode.enclosure_url === playerSrc;
 
-  function playPauseMe() {
-    if (currently_me) {
-      playPause();
-    } else {
-      setCurrentEpisode(episode);
-    }
-  }
+	function playPauseMe() {
+		if (currently_me) {
+			playPause();
+		} else {
+			setCurrentEpisode(episode);
+		}
+	}
 
-  return (
-    <div className="oneEpisode">
-      <img
-        onClick={playPauseMe}
-        src={currently_me && playing ? "/pause.svg" : "/play.svg"}
-        alt={currently_me && playing ? "Mettre en pause" : "Reprendre"}
-      />
-      <p className="OneEpTitle">{episode.title}</p>
-      <p className="OneEpDuration">{convertHMS(episode.enclosure_duration)}</p>
-    </div>
-  );
+	return (
+		<div className="oneEpisode">
+			<img
+				onClick={playPauseMe}
+				src={currently_me && playing ? config.host_assets + "/pause.svg" : host_assets + "/play.svg"}
+				alt={currently_me && playing ? "Mettre en pause" : "Reprendre"}
+			/>
+			<p className="OneEpTitle">{episode.title}</p>
+			<p className="OneEpDuration">{convertHMS(episode.enclosure_duration)}</p>
+		</div>
+	);
 };
 
 export default EpisodesListItem;
