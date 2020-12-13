da0f9918fe5b2f5e8bd9c2a92d6b6439e4887c6f
Bigaston <bigaston1@gmail.com>
2020-12-13 14:01:44 +0100

    Intégration de l'épisode

--- src/page/public/episode.js
+++ src/page/public/episode.js
@@ -5,6 +5,8 @@ import config from "../../config.json"
 import { Link, useHistory } from "react-router-dom"
 import { Helmet } from "react-helmet";
 
+import Toastify from 'toastify-js'
+
 import Loader from "../../component/loader"
 import ToAbout from "../../component/to_about"
 
@@ -122,6 +124,40 @@ export default function EpisodePage() {
 		}
 	}
 
+	// Génération du widget
+	const [modalEmbed, setModalEmbed] = useState(false)
+
+	const [selectedTheme, setSelectedTheme] = useState("white")
+	const [displayEpList, setDisplayEpList] = useState(true);
+	const [playerString, setPlayerString] = useState("");
+	const divResult = useRef();
+	const inputResult = useRef();
+
+	function generatePlayer() {
+		let host = window.location.protocol + "//" + window.location.hostname
+
+		if (!!window.location.port) { host = host + ":" + window.location.port }
+		const chaine = `<iframe src="${host}/player/${episode.slug}?theme=${selectedTheme}${!displayEpList ? "&hide_list=" : ""}" width="100%" style="border: none;" id="muffin_player_div"></iframe><script src="${host}/public/player.js"></script>`
+
+		setPlayerString(chaine);
+		divResult.current.innerHTML = chaine;
+	}
+
+	function clickResult() {
+		inputResult.current.select();
+		document.execCommand('copy');
+
+		Toastify({
+			text: "Widget copié dans votre presse papier!",
+			duration: 1500,
+			newWindow: true,
+			close: true,
+			gravity: "bottom",
+			position: 'center',
+			backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+		}).showToast();
+	}
+
 	return (
 		<>
 			{podcast !== undefined ?
@@ -169,7 +205,7 @@ export default function EpisodePage() {
 									<img src={config.host + "/public/share.svg"} alt="Partager l'épisode" onClick={shareEpisode} />
 								</div>
 								<div ref={divDescription} className="descriptionEp"></div>
-								<p className="moreInfoEp">Publié le {pubDateString}</p>
+								<p className="moreInfoEp">Publié le {pubDateString} - <span onClick={() => { setModalEmbed(true) }} className="openEmbed">Intégrer l'épisode</span></p>
 							</>
 						}
 					</div>
@@ -182,8 +218,32 @@ export default function EpisodePage() {
 						<div className="iconShare">
 							<Icon name="twitter" link={"https://twitter.com/intent/tweet?url=" + encodeURI(window.location.href) + "&text=" + encodeURI(episode.title + " - " + podcast.title)} />
 							<Icon name="facebook" link={"https://www.facebook.com/sharer/sharer.php?u=" + encodeURI(window.location.href)} />
-
 						</div>
+					</Modal>
+
+					<Modal open={modalEmbed} onCancel={() => { setModalEmbed(false) }}>
+						<h2>Intégrer l'épisode</h2>
+						<p>Ici vous pourrez générer un player de podcast que vous pourrez intégrer sur d'autres sites internets. Le player va automatiquement s'adapter à la largeur qu'il dispose.</p>
+						<p>Si il a plus que 490px de large, il sera au format horizontal, si il a moins, il sera au format vertical.</p>
+						<p>Vous pouvez définir vous mêmes la largeur du player, ou alors la laisser à 100% et laisser le script joint. Celui-ci permet de modifier la hauteur de l'intégration en fonction du format, automatiquement.</p>
+						<label htmlFor="type">Thème</label>
+						<select className="u-full-width" id="type" value={selectedTheme} onChange={(event) => { setSelectedTheme(event.target.value) }}>
+							<option value="white">Clair</option>
+							<option value="black">Sombre</option>
+						</select>
+
+						<input type="checkbox" id="explicit" defaultChecked={true} value={displayEpList} onChange={(e) => { setDisplayEpList(e.target.checked) }} />
+						<span className="label-body">Proposer d'afficher la liste des épisodes</span>
+
+						<button className="button-primary" onClick={generatePlayer}>Générer</button>
+
+						{!!playerString ?
+							<>
+								<input ref={inputResult} type="text" className="u-full-width resultInput" readOnly value={playerString} onClick={clickResult} />
+							</>
+							: <></>}
+
+						<div ref={divResult}></div>
 					</Modal>
 				</>
 				: <Loader />}
