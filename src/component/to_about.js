20574cda94aef2c4a3be4d21b33c976cfdf233f4
Bigaston <bigaston1@gmail.com>
2021-01-12 09:18:02 +0100

    Ajout du numéro de version

--- src/component/to_about.js
+++ src/component/to_about.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom"
 
 import "./to_about.css"
+import pkg from "../../package.json";
 
 export default function ToAbout() {
 	return (
-		<p className="toabout">Fonctionne avec <Link to="/about">Muffin</Link></p>
+		<p className="toabout">Fonctionne avec <Link to="/about">Muffin</Link> - v{pkg.version}</p>
 	)
 }
